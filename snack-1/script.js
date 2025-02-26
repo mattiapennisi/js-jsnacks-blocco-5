95b2892eed4f85114c189c33a92e240e60fc4cab
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-26 14:59:02 +0100

    add snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,5 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+const logAllNames = names.forEach(element => console.log(element))
